80315398840f1d36cfbffdc31f3c0c51f94121fe
andres <telescopio322@gmail.com>
2020-09-14 13:43:35 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ChatService } from './services/chat.service';
+import { WebsocketService } from './services/websocket.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng-basic';
+  
+  constructor(
+    public wsService: WebsocketService
+  ) {}
+
 }
